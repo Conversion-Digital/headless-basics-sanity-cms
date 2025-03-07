fe165ac1ae67952f541125994f13bf2e0884f540
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-03-07 14:57:18 +1100

    Grid Something working

--- grid/components/itemValue.tsx
+++ grid/components/itemValue.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Preview, PatchEvent, ObjectInput } from 'sanity'
+import { Preview, PatchEvent, ObjectInput, FormPatch, FieldProps, InputProps, ObjectItemProps, Path, RenderPreviewCallbackProps } from 'sanity'
 import { Dialog, Card, Flex, Button, Stack } from '@sanity/ui'
 import styles from './itemValue.module.css'
 import { getMemberType } from '../utils'
@@ -51,8 +51,8 @@ const RenderItemValue: React.FC<ItemValueProps> = (props) => {
     onChange,
     onRemove,
   } = props
-
   const [isExpanded, setIsExpanded] = useState(false)
+
   const memberType = getMemberType(value, type)
 
   const handleEditStart = () => {
@@ -74,9 +74,8 @@ const RenderItemValue: React.FC<ItemValueProps> = (props) => {
       }
     }
   }
-
-  const handleFieldChange = (patchEvent: PatchEvent) => {
-    onChange(patchEvent, value)
+  const handleFieldChange = (patch: PatchEvent | FormPatch | FormPatch[]) => {
+    onChange(patch as PatchEvent, value)
   }
 
   const renderEditItemForm = (item: any) => {
@@ -101,14 +100,34 @@ const RenderItemValue: React.FC<ItemValueProps> = (props) => {
               schemaType={memberType}
               onChange={handleFieldChange}
               path={[{ _key: item._key }]}
-              focusPath={focusPath}
-              onFocus={onFocus}
-              onBlur={onBlur}
-              readOnly={readOnly || memberType?.readOnly}
-              markers={childMarkers}
-              filterField={filterField}
-            />
-
+              focusPath={focusPath || []}
+              readOnly={readOnly || memberType?.readOnly} groups={[]} onFieldCollapse={function (fieldName: string): void {
+                throw new Error('Function not implemented.')
+              } } onFieldExpand={function (fieldName: string): void {
+                throw new Error('Function not implemented.')
+              } } onFieldSetCollapse={function (fieldSetName: string): void {
+                throw new Error('Function not implemented.')
+              } } onFieldSetExpand={function (fieldSetName: string): void {
+                throw new Error('Function not implemented.')
+              } } onFieldGroupSelect={function (groupName: string): void {
+                throw new Error('Function not implemented.')
+              } } onPathFocus={function (path: Path): void {
+                throw new Error('Function not implemented.')
+              } } onFieldOpen={function (fieldName: string): void {
+                throw new Error('Function not implemented.')
+              } } onFieldClose={function (fieldName: string): void {
+                throw new Error('Function not implemented.')
+              } } renderInput={function (inputProps: Omit<InputProps, 'renderDefault'>): React.ReactNode {
+                throw new Error('Function not implemented.')
+              } } renderField={function (fieldProps: Omit<FieldProps, 'renderDefault'>): React.ReactNode {
+                throw new Error('Function not implemented.')
+              } } renderItem={function (itemProps: Omit<ObjectItemProps, 'renderDefault'>): React.ReactNode {
+                throw new Error('Function not implemented.')
+              } } renderPreview={function (props: RenderPreviewCallbackProps): React.ReactNode {
+                throw new Error('Function not implemented.')
+              } } elementProps={{ id: '', onFocus: () => {}, onBlur: () => {}, ref: React.createRef(), 'aria-describedby': '' }} renderDefault={function (props: InputProps): React.JSX.Element {
+                throw new Error('Function not implemented.')
+              } } members={[]} id={''} level={0} presence={[]} validation={[]} changed={false}            />
             <Flex justify="flex-end" gap={2}>
               {actions.map((action: any) => (
                 <Button
@@ -125,6 +144,18 @@ const RenderItemValue: React.FC<ItemValueProps> = (props) => {
     )
   }
 
+  // If for some reason memberType is not found, skip rendering Preview to avoid crashes
+  if (!memberType) {
+    return (
+      <div
+        className={styles.value_container}
+        style={{color: 'red', fontSize: '0.9rem', padding: '0.5rem'}}
+      >
+        <strong>Cannot render item preview: unknown member type.</strong>
+      </div>
+    )
+  }
+
   return (
     <>
       <div
@@ -132,7 +163,7 @@ const RenderItemValue: React.FC<ItemValueProps> = (props) => {
         tabIndex={0}
         onClick={value._key && handleEditStart}
       >
-        <Preview layout="default" value={value} type={memberType} />
+        <Preview layout="default" value={value} schemaType={memberType} />
       </div>
       {isExpanded && renderEditItemForm(value)}
     </>
