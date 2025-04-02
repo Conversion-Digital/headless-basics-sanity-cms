03d0994b619994d350fcdd22c622050cded06795
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-04-02 11:32:06 +1100

    sanityConfig break out

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID
 console.log("[sanity.cli.ts][7] Sanity Project ID  :", projectId); 
-export default defineCliConfig({
+const sanityConfig = defineCliConfig({
   api: {
     projectId: projectId,
     dataset: 'production'
@@ -17,3 +17,12 @@ export default defineCliConfig({
    */
   autoUpdates: true,
 })
+
+const studoHostSetting = process.env.SANITY_STUDIO_HOST as string
+if(typeof(studoHostSetting) !== "undefined" && studoHostSetting.length > 0) {
+  // Custom handling for studioHost
+  console.log("[sanity.config.ts][149] Studio Host Setting:", studoHostSetting);
+  sanityConfig.studioHost = studoHostSetting;
+}
+
+export default sanityConfig
