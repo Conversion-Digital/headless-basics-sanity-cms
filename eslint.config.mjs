d0781cc53a146fdb2b2b7a03dc35226f9fffe0d6
Tom Tyack <thomas.tyack@conversiondigital.com.au>
2025-04-10 17:24:15 +1000

    latest sanity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
+
 import studio from '@sanity/eslint-config-studio'
 
-export default [...studio]
+export default [
+  ...studio,
+  {
+    extends: ['plugin:next/recommended'],
+  },
+]
+      
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -34,4 +34,4 @@ export const schemaTypes = [
   footerLinkSection,
   footerStructure,
   carousel
-]
+];
